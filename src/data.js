408f4a4e1fe202f50cc4917d3b454c9ea07d47bb
Kundan7634 <1rn21cs083.kundankumar@gmamil.com>
2024-05-29 00:19:24 -0700

    first commit

--- src/data.js
+++ src/data.js
@@ -29,7 +29,7 @@ export const products = [
   {
     id: 3,
     title: "Mens Cotton Jacket",
-    price: 55.99,
+    price: 55.98,
     description:
       "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
     category: "men's clothing",
